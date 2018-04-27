88360b2ce8cfe1b35fee4c7757e1126f0204a96b
Nikita Mazur <mazur.nike@gmail.com>
2018-04-27 12:40:19 +0300

    change db url

--- db/mysql/config.js
+++ db/mysql/config.js
@@ -1,5 +1,5 @@
-{
-    "host": "localhost",
+module.exports = {
+    "host": process.env.CLEARDB_DATABASE_URL,
     "user": "bdc3f29ae6d9b1",
     "password": "0ddec7ae",
     "database": "heroku_a4709875fb65f96"
